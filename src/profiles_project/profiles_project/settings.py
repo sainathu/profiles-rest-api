d9bcf0e7114908d2fdf6ad850e2ad51abd96ad35
manikanta <manikanta.kotagiri@gmail.com>
2018-11-19 11:22:14 +0700

    added server and allowed hosts.

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1b=(ipz2(f-j6hlyy9ley8@8q_c1_#gz^arxn0slu_+b)lbxjy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-18-191-209-189.us-east-2.compute.amazonaws.com']
 
 
 # Application definition
